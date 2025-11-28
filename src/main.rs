862c4e6984beca1459f0083b430c659cfd3517a2
cnowdev <siddhu.vsss@gmail.com>
2025-11-27 23:04:08 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::io::{self, Write};
+
+fn main() {
+    let image_height: u16 = 256;
+    let image_width: u16 = 256;
+
+    print!("P3\n{} {}\n255\n", image_width, image_height);
+
+    for j in 0..image_height {
+        eprint!("\rScanlines remaining: {}", image_height - j);
+        io::stderr().flush().unwrap();
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.0;
+
+            let ir = (255.999 * r) as u16;
+            let ig = (255.999 * g) as u16;
+            let ib = (255.999 * b) as u16;
+
+            print!("{} {} {} \n", ir, ig, ib);
+        }
+    }
+
+    eprintln!("\rDone!                        ");
+}
