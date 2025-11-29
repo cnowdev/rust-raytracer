7e435dc3075006669d55446c85227ce22b97c737
cnowdev <siddhu.vsss@gmail.com>
2025-11-28 22:43:57 -0500

    implement Ray struct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,45 @@
 use std::io::{self, Write};
 pub mod vec3;
 pub mod color;
+pub mod ray;
+
+use color::{Color, write_color};
+use ray::Ray;
+use vec3::{Vec3, Point3};
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = Vec3::unit_vector(&r.direction());
+    let a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0);
+}
 
 fn main() {
-    let image_height: u16 = 256;
-    let image_width: u16 = 256;
+    //Image Constants
+
+    let aspect_ratio: f64 = 16.0 / 9.0;
+    
+    let image_width: u16 = 400;
+    let image_height: u16 = (image_width as f64 / aspect_ratio) as u16;
+
+    // Camera constants
+
+    let focal_length = 1.0;
+    let viewport_height: f64 = 2.0;
+    let viewport_width: f64 = (image_width as f64 / image_height as f64) * viewport_height;
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
+
+    //Viewport vectors
+
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    //deltas from pixel to pixel
+    let pixel_delta_u = viewport_u / (image_width) as f64;
+    let pixel_delta_v = viewport_v / (image_height) as f64;
+
+    // upper left pixel location
+    let viewport_upper_left = camera_center - Vec3::new(0.0,0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
@@ -12,12 +47,12 @@ fn main() {
         eprint!("\rScanlines remaining: {}", image_height - j);
         io::stderr().flush().unwrap();
         for i in 0..image_width {
-            let pixel_color = color::Color::new(
-                i as f64 / (image_width - 1) as f64,
-                j as f64 / (image_height - 1) as f64,
-                0.0,
-            );
-            color::write_color(&mut std::io::stdout(), &pixel_color).unwrap();
+            let pixel_center = pixel00_loc + pixel_delta_u * i as f64 + pixel_delta_v * j as f64;
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(camera_center, ray_direction);
+        
+            let pixel_color = ray_color(&r);
+            write_color(&mut std::io::stdout(), &pixel_color).unwrap();
         }
     }
 
